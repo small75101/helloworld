4c4ce69b1dd92ec41c89da03789081897b7e720b
ranq <751016301@qq.com>
2019-11-26 14:47:07 +0800

    添加打包成war

--- src/main/java/com/small/helloworld/HelloworldApplication.java
+++ src/main/java/com/small/helloworld/HelloworldApplication.java
@@ -2,16 +2,34 @@ package com.small.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 @RestController
-public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        InputStream in = HelloworldApplication.class.getClassLoader().getResourceAsStream("application.properties");
+        properties.load(in);
+        SpringApplication app = new SpringApplication(HelloworldApplication.class);
+        app.setDefaultProperties(properties);
+        app.run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        builder.sources(this.getClass());
+        return super.configure(builder);
     }
 
     @GetMapping("/hello")
